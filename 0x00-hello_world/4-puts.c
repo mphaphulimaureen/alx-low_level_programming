aa603005f945671d5ee46f33c6ebe66c1095244e
mphaphulimaureen <mphaphulimaureen603@gmail.com>
2023-03-16 13:26:18 -0700

    First commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
- *
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return (0);
 }
