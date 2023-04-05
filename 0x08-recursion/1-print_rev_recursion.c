67693af36a9f79e3684b76bcc813b5c0a1f18813
mphaphulimaureen <mphaphulimaureen603@gmail.com>
2023-04-05 12:08:53 -0700

    First commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,11 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _print_rev_recursion - Prints a string in reverse
- * @s: string
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
