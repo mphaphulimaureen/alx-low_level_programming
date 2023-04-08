cd6274425f0b4c2e9e86ce13b7a010678f9003f2
* <mphaphulimaureen693@gmail.com>
2023-04-08 20:55:23 +0200

    First commit

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
-
 /**
+
  * _isupper - uppercase letters
+
  * @c: char to check
+
  *
+
  * Return: 0 or 1
+
  */
 
 
@@ -13,12 +17,12 @@ int _isupper(int c)
 
 {
 
-	        if (c >= 'A' && c <= 'Z')
+        if (c >= 'A' && c <= 'Z')
 
-			                return (1);
+                return (1);
 
-		        else
+        else
 
-				                return (0);
+                return (0);
 
 }
