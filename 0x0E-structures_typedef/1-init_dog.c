0c0229c2c33c96b466050357ab39c088ae7c8e44
* <mphaphulimaureen693@gmail.com>
2023-04-18 23:00:46 +0200

    First commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,21 +1,20 @@
-#ifndef DOG_H 
-#define DOG_H
-
- /** 
-   * init_dog - Initializes a dog structure 
-   * @d: A dog structure. 
-   * @name: The name of the dog. 
-   * @age: The age of the dog. 
-   * @owner: The owner of the dog. 
-   * 
-   * Return: Nothing 
-   */ 
- void init_dog(struct dog *d, char *name, float age, char *owner) 
- { 
+#include "dog.h" 
+#include <stdlib.h> 
+  
+/** 
+ * init_dog - Entry point 
+ * @d: passing structure at the function 
+ * @name: store a strig for main 
+ * @age: store a number for main 
+ * @owner: store a strig for main 
+ */ 
+void init_dog(struct dog *d, char *name, float age, char *owner) 
+{ 
+         /* Passing to stucture from main */ 
          if (d != NULL) 
          { 
                  d->name = name; 
                  d->age = age; 
                  d->owner = owner; 
          } 
- }
+}
