53d3c8f80ab59b09e2019456987b51d4c8d11c58
* <mphaphulimaureen693@gmail.com>
2023-04-18 23:11:03 +0200

    First commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,20 +1,21 @@
-#include "dog.h" 
-#include <stdlib.h> 
-  
-/** 
- * init_dog - Entry point 
- * @d: passing structure at the function 
- * @name: store a strig for main 
- * @age: store a number for main 
- * @owner: store a strig for main 
- */ 
-void init_dog(struct dog *d, char *name, float age, char *owner) 
-{ 
-         /* Passing to stucture from main */ 
-         if (d != NULL) 
-         { 
-                 d->name = name; 
-                 d->age = age; 
-                 d->owner = owner; 
-         } 
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ *
+ * @d: pointer to struct dog
+ * @name: pointer to character array
+ * @age: integer
+ * @owner: pointer to character array
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
