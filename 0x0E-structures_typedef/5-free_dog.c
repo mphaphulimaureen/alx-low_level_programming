aeefed94eb3fea905690f955266654153cc7e5fc
* <mphaphulimaureen693@gmail.com>
2023-04-18 23:52:50 +0200

    First commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,15 +2,18 @@
 #include <stdlib.h> 
   
 /** 
- * free_dog -entry point 
- * @d: entry structure with pointer 
+ * free_dog -frees dogs
+ *
+ * @d: pointer to the 'dog_t' variable to be freed
  */ 
+
 void free_dog(dog_t *d) 
 { 
-         if (d != NULL) 
-         { 
-                 free(d->name); 
-                 free(d->owner); 
-                 free(d); 
-         } 
+  if (d == NULL) 
+  { 
+   return ;
+  }
+  free(d->name); 
+  free(d->owner); 
+  free(d);  
 }
