1de1786bf55c59587f89720abb4af8d129a4fa61
* <mphaphulimaureen693@gmail.com>
2023-04-20 08:56:59 +0200

    First commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,12 @@
 #include "function_pointers.h"
-
 /**
-* print_name - prints name.
-* @name: input name.
-* @f: function pointer.
-*
-* Return: no return
-*/
+ * print_name - prints a name.
+ * @name: received name to pass
+ * @f: pointer to function
+ * Return: Nothing.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-        if (name && f)
-                return;
-        f(name);
+	if (name && f)
+		f(name);
 }
