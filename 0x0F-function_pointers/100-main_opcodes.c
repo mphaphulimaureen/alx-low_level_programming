749a9dac42e09bab23c836ef69a6f74dab5a45c5
mphaphulimaureen <mphaphulimaureen603@gmail.com>
2023-06-26 12:27:05 +0200

    First commit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,37 +1,42 @@
-#include<stdio.h> 
- #include<stdlib.h> 
- /** 
-  * main- Entry point 
- (* a blank line 
-  *@argc: the number of parameters. 
-  *@argv: the parameeters in the case the number ob bytes. 
- * Description: this program prints opcodes in hexa)? 
- * Return: 0 in succes 
- */ 
- int main(int argc, char *argv[]) 
- { 
-         int i, n; 
-  
-  
-         if (argc != 2) 
-         { 
-                 printf("Error\n"); 
-                 return (1); 
-         } 
-         n = atoi(argv[1]); 
-         if (n < 0) 
-         { 
-                 printf("Error\n"); 
-                 exit(2); 
-         } 
-  
-         for (i = 0; i < n; i++) 
-         { 
-                 printf("%02hhx", *((char *)main + i)); 
-                 if (i < n - 1) 
-                         printf(" "); 
-                 else 
-                         printf("\n"); 
-         } 
-         return (0); 
- }
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+        int bytes, i;
+        char *arr;
+
+        if (argc != 2)
+        {
+                printf("Error\n");
+                exit(1);
+        }
+
+        bytes = atoi(argv[1]);
+
+        if (bytes < 0)
+        {
+                printf("Error\n");
+                exit(2);
+        }
+
+        arr = (char *)main;
+
+        for (i = 0; i < bytes; i++)
+        {
+                if (i == bytes - 1)
+                {
+                        printf("%02hhx\n", arr[i]);
+                        break;
+                }
+                printf("%02hhx ", arr[i]);
+        }
+        return (0);
+}
